dcca311dcbdd609ceda28126296ae13b05240b44
AntonioND <antonio_nd@outlook.com>
2015-03-12 12:49:46 +0100

    Finished v1.0 of documentation.
    Finished v1.0 of documentation.

---
+++ doc/sample_code.c
@@ -0,0 +1,249 @@
+
+//--------------------------------------------------------------------
+
+// The actual sensor image is 128x126 or so.
+#define GBCAM_SENSOR_EXTRA_LINES (8)
+#define GBCAM_SENSOR_W (128)
+#define GBCAM_SENSOR_H (112+GBCAM_SENSOR_EXTRA_LINES) 
+
+#define GBCAM_W (128)
+#define GBCAM_H (112)
+
+#define BIT(n) (1<<(n))
+
+// Webcam image
+static int gb_camera_webcam_output[GBCAM_SENSOR_W][GBCAM_SENSOR_H];
+// Image processed by sensor chip
+static int gb_cam_retina_output_buf[GBCAM_SENSOR_W][GBCAM_SENSOR_H];
+
+//--------------------------------------------------------------------
+
+static inline int clamp(int min, int value, int max)
+{
+    if(value < min) return min;
+    if(value > max) return max;
+    return value;
+}
+
+static inline int min(int a, int b) { return (a < b) ? a : b; }
+
+static inline int max(int a, int b) { return (a > b) ? a : b; }
+
+//--------------------------------------------------------------------
+
+static inline u32 gb_cam_matrix_process(u32 value, u32 x, u32 y)
+{
+    x = x & 3;
+    y = y & 3;
+
+    int base = 6 + (y*4 + x) * 3;
+
+    u32 r0 = CAM_REG[base+0];
+    u32 r1 = CAM_REG[base+1];
+    u32 r2 = CAM_REG[base+2];
+
+    if(value < r0) return 0x00;
+    else if(value < r1) return 0x40;
+    else if(value < r2) return 0x80;
+    return 0xC0;
+}
+
+static void GB_CameraTakePicture(void)
+{
+    int i, j;
+
+    //------------------------------------------------
+
+    // Get webcam image
+    // ----------------
+
+    GB_CameraWebcamCapture();
+
+    //------------------------------------------------
+
+    // Get configuration
+    // -----------------
+
+    // Register 0
+    u32 P_bits = 0;
+    u32 M_bits = 0;
+
+    switch( (CAM_REG[0]>>1)&3 )
+    {
+        case 0: P_bits = 0x00; M_bits = 0x01; break;
+        case 1: P_bits = 0x01; M_bits = 0x00; break;
+        case 2: case 3: P_bits = 0x01; M_bits = 0x02; break;
+        default: break;
+    }
+
+    // Register 1
+    u32 N_bit = (CAM_REG[1] & BIT(7)) >> 7;
+    u32 VH_bits = (CAM_REG[1] & (BIT(6)|BIT(5))) >> 5;
+
+    // Registers 2 and 3
+    u32 EXPOSURE_bits = CAM_REG[3] | (CAM_REG[2]<<8);
+
+    // Register 4
+    const float edge_ratio_lut[8] = { 0.50, 0.75, 1.00, 1.25, 2.00, 3.00, 4.00, 5.00 };
+
+    float EDGE_alpha = edge_ratio_lut[(CAM_REG[4] & 0x70)>>4];
+
+    u32 E3_bit = (CAM_REG[4] & BIT(7)) >> 7;
+    u32 I_bit = (CAM_REG[4] & BIT(3)) >> 3;
+
+    //------------------------------------------------
+
+    // Calculate timings
+    // -----------------
+
+    CAM_CLOCKS_LEFT = 4 * ( 32446 + ( N_bit ? 0 : 512 ) + 16 * EXPOSURE_bits );
+
+    //------------------------------------------------
+
+    // Sensor handling
+    // ---------------
+
+    //Copy webcam buffer to sensor buffer applying color correction
+    for(i = 0; i < GBCAM_SENSOR_W; i++) for(j = 0; j < GBCAM_SENSOR_H; j++)
+    {
+        int value = gb_camera_webcam_output[i][j];
+        value = 128 + 0.75f*(float)(value-128);
+        gb_cam_retina_output_buf[i][j] = clamp(0,value,255);
+    }
+
+    // Apply exposure time (with a reference of 0x0100)
+    for(i = 0; i < GBCAM_SENSOR_W; i++) for(j = 0; j < GBCAM_SENSOR_H; j++)
+    {
+        int result = gb_cam_retina_output_buf[i][j];
+        result = ( (result * EXPOSURE_bits ) / 0x0100 );
+        gb_cam_retina_output_buf[i][j] = clamp(0,result,255);
+    }
+
+    if(I_bit) // Invert image
+    {
+        for(i = 0; i < GBCAM_SENSOR_W; i++) for(j = 0; j < GBCAM_SENSOR_H; j++)
+            gb_cam_retina_output_buf[i][j] ^= 255;
+    }
+
+    int temp_buf[GBCAM_SENSOR_W][GBCAM_SENSOR_H];
+
+    u32 filtering_mode = (N_bit<<3) | (VH_bits<<1) | E3_bit;
+    switch(filtering_mode)
+    {
+        case 0x0: // 1-D filtering
+        {
+            for(i = 0; i < GBCAM_SENSOR_W; i++) for(j = 0; j < GBCAM_SENSOR_H; j++)
+            {
+                temp_buf[i][j] = gb_cam_retina_output_buf[i][j];
+            }
+            for(i = 0; i < GBCAM_SENSOR_W; i++) for(j = 0; j < GBCAM_SENSOR_H; j++)
+            {
+                int ms = temp_buf[i][min(j+1,GBCAM_SENSOR_H-1)];
+                int px = temp_buf[i][j];
+
+                int value = 0;
+                if(P_bits&BIT(0)) value += px;
+                if(P_bits&BIT(1)) value += ms;
+                if(M_bits&BIT(0)) value -= px;
+                if(M_bits&BIT(1)) value -= ms;
+                gb_cam_retina_output_buf[i][j] = clamp(0,value,255);
+            }
+            break;
+        }
+        case 0x2: //1-D filtering + Horiz. enhancement : P + {2P-(MW+ME)} * alpha
+        {
+            for(i = 0; i < GBCAM_SENSOR_W; i++) for(j = 0; j < GBCAM_SENSOR_H; j++)
+            {
+                int mw = gb_cam_retina_output_buf[max(0,i-1)][j];
+                int me = gb_cam_retina_output_buf[min(i+1,GBCAM_SENSOR_W-1)][j];
+                int px = gb_cam_retina_output_buf[i][j];
+
+                temp_buf[i][j] = clamp(0,px+((2*px-mw-me)*EDGE_alpha),255);
+            }
+            for(i = 0; i < GBCAM_SENSOR_W; i++) for(j = 0; j < GBCAM_SENSOR_H; j++)
+            {
+                int ms = temp_buf[i][min(j+1,GBCAM_SENSOR_H-1)];
+                int px = temp_buf[i][j];
+
+                int value = 0;
+                if(P_bits&BIT(0)) value += px;
+                if(P_bits&BIT(1)) value += ms;
+                if(M_bits&BIT(0)) value -= px;
+                if(M_bits&BIT(1)) value -= ms;
+                gb_cam_retina_output_buf[i][j] = clamp(0,value,255);
+            }
+            break;
+        }
+        case 0xE: //2D enhancement : P + {4P-(MN+MS+ME+MW)} * alpha
+        {
+            for(i = 0; i < GBCAM_SENSOR_W; i++) for(j = 0; j < GBCAM_SENSOR_H; j++)
+            {
+                int ms = gb_cam_retina_output_buf[i][min(j+1,GBCAM_SENSOR_H-1)];
+                int mn = gb_cam_retina_output_buf[i][max(0,j-1)];
+                int mw = gb_cam_retina_output_buf[max(0,i-1)][j];
+                int me = gb_cam_retina_output_buf[min(i+1,GBCAM_SENSOR_W-1)][j];
+                int px  = gb_cam_retina_output_buf[i][j];
+
+                temp_buf[i][j] = clamp(0,px+((4*px-mw-me-mn-ms)*EDGE_alpha),255);
+            }
+            for(i = 0; i < GBCAM_SENSOR_W; i++) for(j = 0; j < GBCAM_SENSOR_H; j++)
+            {
+                gb_cam_retina_output_buf[i][j] = temp_buf[i][j];
+            }
+            break;
+        }
+        case 0x1:
+        {
+            // In my GB Camera cartridge this is always the same color.
+            // The datasheet of the sensor doesn't have this configuration
+            // documented. Maybe this is a bug?
+            for(i = 0; i < GBCAM_SENSOR_W; i++) for(j = 0; j < GBCAM_SENSOR_H; j++)
+            {
+                gb_cam_retina_output_buf[i][j] = 0x80;
+            }
+            break;
+        }
+        default:
+        {
+            // Ignore filtering
+            printf("Unsupported GB Cam mode: 0x%X\n"
+                   "%02X %02X %02X %02X %02X %02X",
+                   filtering_mode,
+                   CAM_REG[0],CAM_REG[1],CAM_REG[2],
+                   CAM_REG[3],CAM_REG[4],CAM_REG[5]);
+            break;
+        }
+    }
+    
+    //------------------------------------------------
+
+    // Controller handling
+    // -------------------
+
+    int fourcolorsbuffer[GBCAM_W][GBCAM_H]; // buffer after controller matrix
+
+    // Convert to Game Boy colors using the controller matrix
+    for(i = 0; i < GBCAM_W; i++) for(j = 0; j < GBCAM_H; j++)
+        fourcolorsbuffer[i][j] =
+            gb_cam_matrix_process(
+                gb_cam_retina_output_buf[i][j+(GBCAM_SENSOR_EXTRA_LINES/2)],i,j);
+
+    // Convert to tiles
+    u8 finalbuffer[14][16][16]; // final buffer
+    memset(finalbuffer,0,sizeof(finalbuffer));
+    for(i = 0; i < GBCAM_W; i++) for(j = 0; j < GBCAM_H; j++)
+    {
+        u8 outcolor = 3 - (fourcolorsbuffer[i][j] >> 6);
+
+        u8 * tile_base = finalbuffer[j>>3][i>>3];
+        tile_base = &tile_base[(j&7)*2];
+
+        if(outcolor & 1) tile_base[0] |= 1<<(7-(7&i));
+        if(outcolor & 2) tile_base[1] |= 1<<(7-(7&i));
+    }
+
+    // Copy to cart ram...
+    memcpy(&(SRAM[0][0x0100]),finalbuffer,sizeof(finalbuffer));
+}
+
+//--------------------------------------------------------------------
